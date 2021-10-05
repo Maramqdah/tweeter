f42433566e1601854a4ce9f621d38b8e3136a2b0
Maram Qdah <maram.m.qdah@gmail.com>
2021-10-05 17:25:27 -0400

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log( "ready!" );
+  
+  $('#tweet-textarea').keyup(() =>{ 
+    const tweetlength= $('#tweet-textarea').val().length;
+    console.log(tweetlength);
+    $('.counter').text(140-tweetlength);
+
+  })
+ 
+  
+});
