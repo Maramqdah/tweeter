48216a1631f0e5ad221685a38d277a33e304165e
Maram Qdah <maram.m.qdah@gmail.com>
2021-10-05 21:24:54 -0400

    creating tweets edit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  //console.log( "ready!" );
-  
-  $('#tweet-textarea').keyup(() =>{ 
-    const tweetlength= $('#tweet-textarea').val().length;
-    console.log(tweetlength);
-    $('.counter').text(140-tweetlength);
+    // --- our code goes here ---
+    //console.log( "ready!" );
 
-  })
+    $('#tweet-textarea').keyup(() => {
+        const tweetlength = $('#tweet-textarea').val().length;
+        console.log(tweetlength);
+        $('.counter').text(140 - tweetlength);
+
+    })
+    $(selector).mouseenter(handlerIn).mouseleave(handlerOut);
 });
