f8197c9174ecd8d0cd9e66ff2859b9eaffe01257
Maram Qdah <maram.m.qdah@gmail.com>
2021-10-06 19:20:06 -0400

    form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,32 +29,7 @@ $(() => {
 
     }
 
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const data = [{
-            "user": {
-                "name": "Newton",
-                "avatars": "https://i.imgur.com/73hZDYK.png",
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": "https://i.imgur.com/nlhLi3I.png",
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        }
-    ]
 
-    //===============================
     const renderTweets = function(tweets) {
         for (const tweet of tweets) {
             const $tweet = createTweetElement(tweet);
@@ -62,7 +37,6 @@ $(() => {
         }
 
     }
-    renderTweets(data);
 
 
     const $form = $(".new-tweet-form");
@@ -74,16 +48,15 @@ $(() => {
 
     })
 
-
     //Fetching tweets with Ajax
     const loadTweets = () => {
         $.ajax({
             url: "/tweets",
             method: "GET",
             dataType: "json",
-            success: (tweet) => {
-                console.log("tweet:", tweet);
-                createTweetElement(tweet);
+            success: (tweets) => {
+                console.log("tweet:", tweets);
+                renderTweets(tweets);
             },
             error: (err) => {
                 console.log(`there was an error: ${err}`)
@@ -91,6 +64,6 @@ $(() => {
         })
     }
 
-
+    loadTweets();
 
 });
